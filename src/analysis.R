caea83664981adbd7dbcaf76d4b01d43439a6d0d
Alden Chen <alden.chen@outlook.com>
2019-04-11 21:31:18 -0700

    Add polr model

--- src/analysis.R
+++ src/analysis.R
@@ -1,5 +1,15 @@
 library(tidyverse)
 library(broom)
-data <- read_csv("cleaned_data.csv") %>% filter(os != "Linux")
-tidy(anova(lm(stress ~ os + coding + background + work_year + age + gender, data = data)))
+library(MASS)
+
+data <- read_csv("cleaned_data.csv")
+
+# linear regression with stress and os only
+tidy(anova(lm(stress ~ os, data = data)))
+
+# linear regression with coding and background
+tidy(anova(lm(stress ~ os + coding + background, data = data)))
+
+# proportional odds model, stress and os only
+tidy(polr(as.factor(stress) ~ os, data = data))
 
